4a7c5db04bec5b251d7bd5c54a4aeabbd13ecfb0
Elena Sveshnikova <lenamarinkevich@mail.ru>
2022-11-09 15:44:46 +0300

    fear: class Popup done

--- scripts/Popup.js
+++ scripts/Popup.js
@@ -17,21 +17,20 @@ export default class Popup {
         document.querySelector(this._selectorPopup).classList.remove('popup_opened');
     }
 
-    // not work
     _handeListenEscape = (evt) => {
         if (evt.key === 'Escape') {
             this.close();
           };
     }
 
-    _closeOnOverlay = (evt) => {
+    _handleListenOverlay = (evt) => {
         if (evt.target.classList.contains('popup')){
             this.close();
           }
     }
 
     setEventListeners() {
-        document.querySelector(this._selectorPopup).addEventListener('click', this._closeOnOverlay)
+        document.querySelector(this._selectorPopup).addEventListener('click', this._handleListenOverlay)
         document.addEventListener('keydown', this._handeListenEscape);
         document.querySelector(this._selectorPopup).querySelector('.popup__btn-close')
                 .addEventListener('click', this.close)
