b61ddb3d5a271b417ce6a8a95780a2bc1c25940a
Elena Sveshnikova <lenamarinkevich@mail.ru>
2022-12-05 13:39:25 +0300

    pull

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -14,21 +14,13 @@ export default class Popup {
     _setEventListeners() {
         document    .addEventListener('keydown', this._handeListenEscape);
         this._popup .addEventListener('click', this._handleListenOverlay);
-<<<<<<< HEAD
         this._btnClose.addEventListener('click', this._closeButton = () => this.close());
-=======
-        this._btnClose.addEventListener('click', () =>  this.close());
->>>>>>> dd1b5035f0f037d7623f5c4caf78b1ddd2a5de7a
     }
 
     _removeEventListeners() {
         document    .removeEventListener('keydown', this._handeListenEscape);
         this._popup .removeEventListener('click', this._handleListenOverlay);
-<<<<<<< HEAD
         this._btnClose.removeEventListener('click', this._closeButton);
-=======
-        this._btnClose.removeEventListener('click', () => this.close());
->>>>>>> dd1b5035f0f037d7623f5c4caf78b1ddd2a5de7a
     }
 
     open() {
